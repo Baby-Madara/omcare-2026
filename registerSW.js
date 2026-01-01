afcb789f59eec7cd08a2e79cf9b0fd5e69837268
Ahmed Farahat <75583175+Baby-Madara@users.noreply.github.com>
2026-01-01 10:25:23 +0200

    Add files via upload

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/omcare-2026/sw.js', { scope: '/omcare-2026/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
